9e90c45d89a83c6c60f4e108de0b9b1b9d3ece92
huongnguyenduc <huong@codelight.co>
2022-10-11 18:47:33 +0700

    feat: add textfield component

---
+++ apps/web/src/components/molecules/text-field/text-field.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import React from 'react';
+import { Input, InputProps } from '../../atoms/input/input';
+
+const NAME = 'TextField';
+
+export interface TextFieldProps extends InputProps {
+  help?: string;
+  action?: React.ReactNode;
+}
+
+export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ help, title, error, className, action, disabled, ...rest }, ref) => {
+    const id = React.useId();
+    return (
+      <div className={clsx('w-full', className)}>
+        {(title || action) && (
+          <div className="mb-2 flex w-full items-center justify-between">
+            <label htmlFor={id} className="text-neutral font-semibold capitalize" data-component={'title'}>
+              {title}
+            </label>
+            {action && <div data-component={'action'}>{action}</div>}
+          </div>
+        )}
+        <Input id={id} ref={ref} {...rest} error={error} disabled={disabled} data-component={'input'} />
+        {(error || help) && (
+          <div
+            className={clsx(error ? 'text-accent-error' : 'text-neutral-50', 'mt-2.5 px-3 text-xs font-normal')}
+            data-component={'helper-text'}
+          >
+            {error ? !disabled && error : help}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+TextField.displayName = NAME;
