6aa1a4beef2ad75b59acfc1f09665ec1775dc156
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2022-10-11 18:07:54 +0700

    feat: add graphics svg

---
+++ apps/web/src/components/icons/graphic/index.ts
@@ -0,0 +1,31 @@
+import checkMail from './check-mail.svg';
+import comingSoon from './coming-soon.svg';
+import empty from './empty.svg';
+import error403 from './error-403.svg';
+import error404 from './error-404.svg';
+import error500 from './error-500.svg';
+import error from './error.svg';
+import loading from './loading.svg';
+import maintenanceSystem from './maintenance-system.svg';
+import noInternet from './no-internet.svg';
+import paymentError from './payment-error.svg';
+import paymentSuccess from './payment-success.svg';
+import pcLaptop from './pc-laptop.svg';
+import success from './success.svg';
+
+export {
+  checkMail as CheckMailGraphic,
+  comingSoon as ComingSoonGraphic,
+  empty as EmptyGraphic,
+  error as ErrorGraphic,
+  error403 as Error403Graphic,
+  error404 as Error404Graphic,
+  error500 as Error500Graphic,
+  loading as LoadingGraphic,
+  maintenanceSystem as MaintenanceSystemGraphic,
+  noInternet as NoInternetGraphic,
+  paymentError as PaymentErrorGraphic,
+  paymentSuccess as PaymentSuccessGraphic,
+  pcLaptop as PcLaptopGraphic,
+  success as SuccessGraphic
+};
