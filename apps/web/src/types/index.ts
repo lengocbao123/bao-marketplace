e6a049dc9dcb9b167430bd6483483bba368989a9
huongnguyenduc <huong@codelight.co>
2022-10-13 17:43:50 +0700

    chore: add user form interface

--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -1,3 +1,11 @@
 import { FC, SVGProps } from 'react';
 
 export type SVG = FC<SVGProps<SVGSVGElement>>;
+
+export interface UserForm<T, R> {
+  show?: boolean;
+
+  onSuccess?: (formData: T, response?: R) => void;
+
+  onError?: (error: any) => void;
+}
