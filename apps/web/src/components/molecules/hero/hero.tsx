8949f5e00c8a1643fcee98d56c0134def9a98afa
huongnguyenduc <huong@codelight.co>
2022-10-17 19:02:34 +0700

    feat: add content to hero component

--- apps/web/src/components/molecules/hero/hero.tsx
+++ apps/web/src/components/molecules/hero/hero.tsx
@@ -1,41 +1,45 @@
 import Image from 'next/future/image';
 import { FC, HTMLAttributes } from 'react';
+import { Button } from '../../atoms';
+import { ChevronRightIcon } from '../../icons/outline';
 
 export type HeroProps = HTMLAttributes<HTMLElement>;
 
 export const Hero: FC<HeroProps> = (props) => {
   return (
-    <section className={'bg-white'} {...props}>
+    <section
+      className={'bg-[url(/assets/images/hero/hero-background.png)] bg-contain bg-center bg-no-repeat md:bg-cover'}
+      {...props}
+    >
       <div className="gap-7.5 container grid overflow-hidden md:grid-cols-2">
         <div className="py-20">
-          {/* TODO: add more here */}
-          <div className="hidden lg:block">
-            {' '}
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci alias culpa dolorem ducimus
-            facere, ipsa iste itaque iure magnam maiores necessitatibus non nostrum officia perferendis ratione rerum
-            sed veritatis vero vitae. Aliquid aut consequatur cumque fugit, impedit ipsam iste itaque maiores molestiae
-            neque pariatur possimus quaerat quod unde voluptates. Illum molestias obcaecati voluptatum? Ab accusantium
-            aliquam at beatae consequatur corporis cum debitis dolor ea eius eos excepturi explicabo fugiat illo in
-            ipsum iusto laborum laudantium maxime nam nemo nulla, numquam officiis possimus praesentium provident quia
-            quidem quisquam quod reiciendis rerum similique sunt temporibus totam unde veniam, voluptatum! Ab accusamus
-            ad, adipisci animi architecto assumenda at cum deserunt dicta eius eos facere, fuga fugit hic impedit in
-            iste itaque labore laboriosam libero nisi nobis nostrum odit porro praesentium provident, quaerat quia
-            quibusdam recusandae repellendus reprehenderit sequi sint tempore tenetur vel veniam voluptatem. Ab ad animi
-            architecto, autem dolorum explicabo fuga fugit in ipsa ipsam iure iusto magnam minima minus mollitia nulla
-            obcaecati odio omnis pariatur quae rem sequi similique soluta tempora voluptas! Iure molestias quia quis
-            soluta. Ab atque beatae commodi consequuntur debitis distinctio error excepturi fugit illo, incidunt ipsam
-            nostrum perspiciatis, placeat quod quos reiciendis repellendus ullam vel voluptatum!
+          <h1 className={'text-4xl font-bold lg:text-5xl xl:text-6xl'}>
+            Discover,
+            <br /> Collect and Sell
+            <br /> Extraordinary NFTs
+            <br />
+            <div
+              className={
+                'relative mt-2 w-fit bg-gradient-to-tr from-[#DA22FF] to-[#6E43D4] bg-clip-text text-transparent md:mt-1'
+              }
+            >
+              by Credit Card
+              <div
+                className={'md:h-0.75 bottom-0.75 absolute h-0.5 w-full bg-gradient-to-r from-[#DA22FF] to-[#6E43D4]'}
+              />
+            </div>
+          </h1>
+          <div className={'mt-7.5 text-sm text-neutral-50 lg:text-base xl:text-lg'}>
+            The largest NFT marketplace. Authentic and truly unique digital creation. Signed and issued by the creator,
+            made possible by blockchain technology.
           </div>
-          <div className="lg:hidden">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem blanditiis culpa cum dolorem,
-            doloribus ducimus ea facere id molestias nemo, nesciunt nobis numquam, porro possimus quaerat quisquam
-            recusandae rem reprehenderit sequi sint suscipit totam vero! Accusamus adipisci architecto, assumenda
-            blanditiis cupiditate, dignissimos dolor eligendi enim eos eveniet ex facere fugit laborum nostrum obcaecati
-            omnis optio quaerat quisquam reiciendis repellendus repudiandae soluta veniam voluptatem! Aliquam commodi,
-            eveniet exercitationem harum inventore maiores ratione rem veniam! Ab dolorum eum hic magni nulla ratione.
-            Aliquid cum cumque cupiditate earum eius enim, et eum explicabo hic, impedit neque obcaecati officia porro
-            quae sit. Vero!
-          </div>
+          <Button
+            label={'Explore The Marketplace'}
+            className={'xl:mt-15 mt-10 !py-2.5 !text-sm md:mt-12 lg:!py-3 lg:!text-base xl:!text-lg'}
+            icon={ChevronRightIcon}
+            iconOrientation={'right'}
+            size={'lg'}
+          />
         </div>
         <div className="relative hidden overflow-hidden md:block">
           <div className="absolute right-0">
