385e07d4ce8599515d2403e952c16a76cdfa01cd
huongnguyenduc <huong@codelight.co>
2022-10-25 21:26:57 +0700

    chore: update attributes in CheckboxInput

--- apps/web/src/components/atoms/input/checkbox.tsx
+++ apps/web/src/components/atoms/input/checkbox.tsx
@@ -4,6 +4,7 @@ import { FC, InputHTMLAttributes, ReactNode } from 'react';
 export interface CheckboxInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: ReactNode;
 }
+
 export const CheckboxInput: FC<CheckboxInputProps> = ({ label, disabled, className, ...rest }) => {
   return (
     <label className={clsx('flex items-center', className)}>
@@ -11,12 +12,14 @@ export const CheckboxInput: FC<CheckboxInputProps> = ({ label, disabled, classNa
         type="checkbox"
         disabled={disabled}
         className={'h-4.5 w-4.5 border-neutral-30 mr-3 rounded border-2 focus:ring-0 focus:ring-offset-0'}
+        data-component={'checkbox'}
         {...rest}
       />
       <div
         className={clsx('select-none', {
           'text-neutral-30': disabled
         })}
+        data-component={'label'}
       >
         {label}
       </div>
