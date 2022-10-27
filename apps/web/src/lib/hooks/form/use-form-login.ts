20705b343b793f23f9c5142448258f382ecdbc76
huongnguyenduc <duchuong007@gmail.com>
2022-10-27 18:11:28 +0700

    feat: add login validate form

---
+++ apps/web/src/lib/hooks/form/use-form-login.ts
@@ -0,0 +1,60 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { UserForm } from '../../../types';
+
+export interface UseFormLogin {
+  email: string;
+  password: string;
+}
+
+export const useFormLogin = (
+  options: { initialData?: UseFormLogin } & UserForm<UseFormLogin, any>
+) => {
+  const schema = yup.object().shape({
+    email: yup.string().email("Email is invalid").required("Email is required"),
+    password: yup.string().required("Password is required"),
+  });
+
+  const defaultValues = useMemo(() => options.initialData, [options.initialData]);
+
+  const methods = useForm<UseFormLogin>({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+    reValidateMode: 'onChange',
+    defaultValues
+  });
+
+  const onSubmit = async (formData: UseFormLogin) => {
+    try {
+      const body = new FormData();
+      body.append('email', formData.email);
+      body.append('password', formData.password);
+
+      // Wait for 1 second to simulate a slow network
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // Random result to simulate a random error
+      const result =
+        Math.random() > 0.5
+          ? { success: true, statusCode: 200 }
+          : {
+              success: false,
+              message: 'Internal server error',
+              statusCode: 500
+            };
+      if (result.statusCode === 200) {
+        await options?.onSuccess?.(formData, result);
+      } else {
+        await options?.onError?.(`StatusCode: ${result.statusCode}. message: ${result.message}`);
+      }
+    } catch (error) {
+      await options?.onError?.(error);
+    }
+  };
+
+  return {
+    ...methods,
+    onSubmit: methods.handleSubmit(onSubmit)
+  };
+};
